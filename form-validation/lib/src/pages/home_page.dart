0ef146aeb10942ae27e98bedfb856a410e56ca09
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2020-02-06 13:34:50 -0600

    cargar casas
    método para traer de firebase y decodificar el json para asignarlo al modelo

--- form-validation/lib/src/pages/home_page.dart
+++ form-validation/lib/src/pages/home_page.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/bloc/provider.dart';
+import 'package:formvalidation/src/providers/casa_provider.dart';
+
+import '../models/casa_model.dart';
 
 class HomePage extends StatelessWidget {
+  final casasProvider = new CasaProvider();
+
   @override
   Widget build(BuildContext context) {
-
     final bloc = Provider.of(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home')
-      ),
-      body: Container(),
+      appBar: AppBar(title: Text('Home')),
+      body: _crearListado(),
       floatingActionButton: _crearBoton(context),
+    );
+  }
+
+  _crearBoton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () => Navigator.pushNamed(context, 'casa'),
+      backgroundColor: Color.fromRGBO(222, 97, 97, 1.0),
+    );
+  }
+
+  Widget _crearListado() {
+    return FutureBuilder(
+      future: casasProvider.cargarCasas(),
+      builder: (BuildContext context, AsyncSnapshot<List<CasaModel>> snapshot) {
+        if (snapshot.hasData) {
+          return Container();
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
           );
         }
-      
-        _crearBoton(BuildContext context) {
-          return FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: ()=> Navigator.pushNamed(context, 'casa'),
-            backgroundColor: 
-            Color.fromRGBO(222, 97, 97, 1.0),
-          
-          );
-        }
+      },
+    );
+  }
 }
--- form-validation/lib/src/providers/casa_provider.dart
+++ form-validation/lib/src/providers/casa_provider.dart
@@ -1,21 +1,37 @@
-
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
 import '../models/casa_model.dart';
+import '../models/casa_model.dart';
+import '../models/casa_model.dart';
 
 class CasaProvider {
-  final String _url =  "https://flutter-varios-bc255.firebaseio.com";
-  
-  Future<bool> crearCasa(CasaModel casa) async{
-  final url = '$_url/casas.json';
+  final String _url = "https://flutter-varios-bc255.firebaseio.com";
 
-  final resp = await http.post(url, body: casaModelToJson(casa));
+  Future<bool> crearCasa(CasaModel casa) async {
+    final url = '$_url/casas.json';
+    final resp = await http.post(url, body: casaModelToJson(casa));
+    final decodedData = json.decode(resp.body);
+    print(decodedData);
+    return true;
+  }
 
-  final decodedData = json.decode(resp.body);
-  print(decodedData);
-  return true;
+  Future<List<CasaModel>> cargarCasas() async {
+    final url = '$_url/casas.json';
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(resp.body);
+    final List<CasaModel> casas = List();
+
+    if (decodedData == null) return [];
+    decodedData.forEach((id, casa) {
+      final casaTemp = CasaModel.fromJson(casa);
+      casaTemp.id = id;
+      casas.add(casaTemp);
+    });
+
+    print(casas);
+    return casas;
   }
 }
